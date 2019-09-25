47b2004378c66cd9c338c953c38aea06bf08b570
Neos21 <neos21@gmail.com>
2019-09-26 00:17:56 +0900

    3カメまではプレビューできるが、4カメ目を設定しようとすると DeviceInput の addInput ができずに失敗する。

--- MultiCam/ViewController.swift
+++ MultiCam/ViewController.swift
@@ -12,7 +12,7 @@ import AVFoundation
 
 class ViewController: UIViewController {
   // セッション
-  var session: AVCaptureSession!
+  var session: AVCaptureMultiCamSession!
   
   // 超広角 : 左上
   @IBOutlet var ultraWideUIView: UIView!
@@ -32,6 +32,24 @@ class ViewController: UIViewController {
   // 広角 : Output
   var wideAngleOutput: AVCaptureMovieFileOutput!
   
+  // 望遠 : 左下
+  @IBOutlet var telephotoUIView: UIView!
+  // 望遠 : プレビューレイヤー
+  var telephotoPreviewLayer: AVCaptureVideoPreviewLayer!
+  // 望遠 : Input
+  var telephotoInput: AVCaptureDeviceInput!
+  // 望遠 : Output
+  var telephotoOutput: AVCaptureMovieFileOutput!
+  
+  // フロント : 右下
+  @IBOutlet var frontUIView: UIView!
+  // フロント : プレビューレイヤー
+  var frontPreviewLayer: AVCaptureVideoPreviewLayer!
+  // フロント : Input
+  var frontInput: AVCaptureDeviceInput!
+  // フロント : Output
+  var frontOutput: AVCaptureMovieFileOutput!
+  
   // 初期表示時の処理
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -47,14 +65,22 @@ class ViewController: UIViewController {
     // AVCaptureMultiCamSession を使う時は手動で AVCaptureInputs から AVCaptureOutputs にコネクションを繋ぐと良いらしい
     self.session.beginConfiguration()
     
-    guard setupBackUltraWideCamera() else {
+    guard setupUltraWideCamera() else {
       print("超広角カメラの準備に失敗")
       return
     }
-    guard setupBackWideAngleCamera() else {
+    guard setupWideAngleCamera() else {
       print("広角カメラの準備に失敗")
       return
     }
+    guard setupTelephotoCamera() else {
+      print("望遠カメラの準備に失敗")
+      return
+    }
+    // guard setupFrontCamera() else {
+    //   print("フロントカメラの準備に失敗")
+    //   return
+    // }
     
     self.session.commitConfiguration()
     self.session.startRunning()
@@ -62,7 +88,7 @@ class ViewController: UIViewController {
   }
   
   // 超広角カメラを用意する
-  private func setupBackUltraWideCamera() -> Bool {
+  private func setupUltraWideCamera() -> Bool {
     self.session.beginConfiguration()
     defer {
       self.session.commitConfiguration()
@@ -127,13 +153,12 @@ class ViewController: UIViewController {
     }
     self.session.addConnection(layerConnection)
     
-    
     print("[超広角] 準備完了")
     return true
   }
   
   // 広角カメラを用意する
-  private func setupBackWideAngleCamera() -> Bool {
+  private func setupWideAngleCamera() -> Bool {
     self.session.beginConfiguration()
     defer {
       self.session.commitConfiguration()
@@ -196,8 +221,149 @@ class ViewController: UIViewController {
     }
     self.session.addConnection(layerConnection)
     
-    
     print("[広角] 準備完了")
+    return true
+  }
+  
+  // 望遠カメラを用意する
+  private func setupTelephotoCamera() -> Bool {
+    self.session.beginConfiguration()
+    defer {
+      self.session.commitConfiguration()
+    }
+    
+    guard let videoDevice = AVCaptureDevice.default(AVCaptureDevice.DeviceType.builtInTelephotoCamera, for: AVMediaType.video, position: AVCaptureDevice.Position.back) else {
+      print("[望遠] VideoDevice 生成に失敗")
+      return false
+    }
+    
+    // Input を Session に追加する
+    do {
+      self.telephotoInput = try AVCaptureDeviceInput(device: videoDevice)
+      guard session.canAddInput(self.telephotoInput) else {
+        print("[望遠] Input を Session に追加できない")
+        return false
+      }
+      self.session.addInputWithNoConnections(self.telephotoInput)
+    }
+    catch {
+      print("[望遠] Input 準備に失敗 : \(error)")
+      return false
+    }
+    
+    // Input からビデオポートを見つける
+    guard let videoPort = self.telephotoInput.ports(for: AVMediaType.video, sourceDeviceType: videoDevice.deviceType, sourceDevicePosition: videoDevice.position).first else {
+      print("[望遠] ビデオポートを見つけられなかった")
+      return false
+    }
+    
+    // Output を Session に追加する
+    self.telephotoOutput = AVCaptureMovieFileOutput()
+    guard self.session.canAddOutput(self.telephotoOutput) else {
+      print("[望遠] Output を Session に追加できない")
+      return false
+    }
+    self.session.addOutputWithNoConnections(self.telephotoOutput)
+    
+    // Input と Output を接続する
+    let outputConnection = AVCaptureConnection(inputPorts: [videoPort], output: self.telephotoOutput)
+    guard self.session.canAddConnection(outputConnection) else {
+      print("[望遠] Input と Output を接続できなかった")
+      return false
+    }
+    self.session.addConnection(outputConnection)
+    outputConnection.videoOrientation = AVCaptureVideoOrientation.portrait
+    
+    // ビデオポートをプレビューレイヤーに接続する
+    self.telephotoPreviewLayer = AVCaptureVideoPreviewLayer()
+    self.telephotoPreviewLayer.setSessionWithNoConnection(self.session)
+    self.telephotoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspect  // アスペクトフィット
+    self.telephotoPreviewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+    self.telephotoUIView.layer.addSublayer(self.telephotoPreviewLayer)
+    self.telephotoPreviewLayer.position = CGPoint(x: self.telephotoUIView.frame.width / 2, y: self.telephotoUIView.frame.height / 2)
+    self.telephotoPreviewLayer.bounds = self.telephotoUIView.frame
+    let layerConnection = AVCaptureConnection(inputPort: videoPort, videoPreviewLayer: self.telephotoPreviewLayer)
+    guard self.session.canAddConnection(layerConnection) else {
+      print("[望遠] ビデオポートをプレビューレイヤーに接続できなかった")
+      return false
+    }
+    self.session.addConnection(layerConnection)
+    
+    print("[望遠] 準備完了")
+    return true
+  }
+  
+  // フロントカメラを用意する
+  private func setupFrontCamera() -> Bool {
+    self.session.beginConfiguration()
+    defer {
+      self.session.commitConfiguration()
+    }
+    
+    guard let videoDevice = AVCaptureDevice.default(AVCaptureDevice.DeviceType.builtInWideAngleCamera, for: AVMediaType.video, position: AVCaptureDevice.Position.front) else {
+      print("[フロント] VideoDevice 生成に失敗")
+      return false
+    }
+    
+    // Input を Session に追加する
+    do {
+      self.frontInput = try AVCaptureDeviceInput(device: videoDevice)
+      guard session.canAddInput(self.frontInput) else {
+        print("[フロント] Input を Session に追加できない")
+        return false
+      }
+      self.session.addInputWithNoConnections(self.frontInput)
+      // Terminating app due to uncaught exception 'NSInvalidArgumentException', reason: '*** -[AVCaptureMultiCamSession addInputWithNoConnections:] These devices may not be used simultaneously. Use -[AVCaptureDeviceDiscoverySession supportedMultiCamDeviceSets]'
+      // 3つまでは同時起動できるが、4つ目はどの組合せでも表示できない
+    }
+    catch {
+      print("[フロント] Input 準備に失敗 : \(error)")
+      return false
+    }
+    
+    // Input からビデオポートを見つける
+    guard let videoPort = self.frontInput.ports(for: AVMediaType.video, sourceDeviceType: videoDevice.deviceType, sourceDevicePosition: videoDevice.position).first else {
+      print("[フロント] ビデオポートを見つけられなかった")
+      return false
+    }
+    
+    // Output を Session に追加する
+    self.frontOutput = AVCaptureMovieFileOutput()
+    guard self.session.canAddOutput(self.frontOutput) else {
+      print("[フロント] Output を Session に追加できない")
+      return false
+    }
+    self.session.addOutputWithNoConnections(self.frontOutput)
+    
+    // Input と Output を接続する
+    let outputConnection = AVCaptureConnection(inputPorts: [videoPort], output: self.frontOutput)
+    guard self.session.canAddConnection(outputConnection) else {
+      print("[フロント] Input と Output を接続できなかった")
+      return false
+    }
+    self.session.addConnection(outputConnection)
+    outputConnection.videoOrientation = AVCaptureVideoOrientation.portrait
+    outputConnection.automaticallyAdjustsVideoMirroring = false
+    outputConnection.isVideoMirrored = false  // 鏡写しにするかどうか
+    
+    // ビデオポートをプレビューレイヤーに接続する
+    self.frontPreviewLayer = AVCaptureVideoPreviewLayer()
+    self.frontPreviewLayer.setSessionWithNoConnection(self.session)
+    self.frontPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspect  // アスペクトフィット
+    self.frontPreviewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait  // TODO : ココから3つは layerConnection 宣言後に指定するモノかもしれない
+    self.frontPreviewLayer.connection?.automaticallyAdjustsVideoMirroring = false
+    self.frontPreviewLayer.connection?.isVideoMirrored = false  // 鏡写しにするかどうか
+    self.frontUIView.layer.addSublayer(self.frontPreviewLayer)
+    self.frontPreviewLayer.position = CGPoint(x: self.frontUIView.frame.width / 2, y: self.frontUIView.frame.height / 2)
+    self.frontPreviewLayer.bounds = self.frontUIView.frame
+    let layerConnection = AVCaptureConnection(inputPort: videoPort, videoPreviewLayer: self.frontPreviewLayer)
+    guard self.session.canAddConnection(layerConnection) else {
+      print("[フロント] ビデオポートをプレビューレイヤーに接続できなかった")
+      return false
+    }
+    self.session.addConnection(layerConnection)
+    
+    print("[フロント] 準備完了")
     return true
   }
   
