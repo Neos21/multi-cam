fc3e6c5e53c9c978e16971918b91e5ffb3193121
Neos21 <neos21@gmail.com>
2019-09-25 16:12:37 +0900

    Initial Commit

---
+++ MultiCam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiCam
+//
+//  Created by Neo on 2019-09-25.
+//  Copyright © 2019 Neo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
